3a7b7a872e84a95bde4f1d77b2c4d866b86062bf
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-05 09:57:43 -0500

    Enrutando el componente ventas y el componente compras

--- p09routing/src/app/app.routes.ts
+++ p09routing/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { VentasComponent } from './ventas/ventas.component';
+import { ComprasComponent } from './compras/compras.component';
 
 export const routes: Routes = [
-    {path:'', component: InicioComponent} // La dejaremos vacía por que va a ser la ruta de inicio.
+    {path:'', component: InicioComponent}, // La dejaremos vacía por que va a ser la ruta de inicio.
+    {path:'ventas', component: VentasComponent},
+    {path:'compras', component: ComprasComponent}
 ];
