50432581d80d3ba2cde1ad240eb2b1c42b7293d9
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-05 11:58:04 -0500

    Importando el módulo de RouterModule para hacer uso de su directiva routerLink que nos permite una vez se de clic en el botón redirigirnos a la ruta que se le coloco previamente

--- p09routing/src/app/compras/compras.component.ts
+++ p09routing/src/app/compras/compras.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-compras',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './compras.component.html',
   styleUrl: './compras.component.css'
 })
