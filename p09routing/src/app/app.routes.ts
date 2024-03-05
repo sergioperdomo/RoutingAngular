9de35001087804202f1faada724c28995208c7f3
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-05 09:36:45 -0500

    Añadiendo la ruta de nuestro nuevo componente, en nuestro fichero app.routes.ts

--- p09routing/src/app/app.routes.ts
+++ p09routing/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: InicioComponent} // La dejaremos vacía por que va a ser la ruta de inicio.
+];
