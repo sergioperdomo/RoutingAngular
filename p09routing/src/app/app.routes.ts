2f99961db5889fae9706851ef14d0cc5ef140e64
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-03-06 09:43:22 -0500

    Añadiendo una nueva ruta, que es alusiva a error de digitación de ruta en el navegador por eso se ponen **

--- p09routing/src/app/app.routes.ts
+++ p09routing/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { VentasComponent } from './ventas/ventas.component';
 import { ComprasComponent } from './compras/compras.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
 export const routes: Routes = [
     {path:'', component: InicioComponent}, // La dejaremos vacía por que va a ser la ruta de inicio.
     {path:'ventas', component: VentasComponent},
-    {path:'compras', component: ComprasComponent}
+    {path:'compras', component: ComprasComponent},
+    {path:'**', component: Pagina404Component}
 ];
